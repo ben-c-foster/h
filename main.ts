a2f2852621663a6c625b73c64298bb2e39c95518
ben-c-foster <76908598+ben-c-foster@users.noreply.github.com>
2022-02-09 07:36:35 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . . . 
+    c c c c c d d e e e f c . . . . 
+    . f d d d d e e e f f . . . . . 
+    . . f e e e f f e e e f . . . . 
+    . . f f f f f e e e e e f . f f 
+    . . f d b f e e f f e e f . e f 
+    . f f d d f e f f e e e f . e f 
+    . f f f f f f e b b f e f f e f 
+    . f d d f e e e d d b e f f f f 
+    . . f f f f f f f f f f f f f . 
+    `, SpriteKind.Player)
